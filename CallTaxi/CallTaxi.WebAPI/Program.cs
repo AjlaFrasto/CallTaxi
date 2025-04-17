1ad9de99f1ea76378f5844a67505f746ed7b0ddb
Ajla Frašto <ajlafrasto@hotmail.com>
2025-04-17 23:49:46 +0200

    Services configuration

--- CallTaxi/CallTaxi.WebAPI/Program.cs
+++ CallTaxi/CallTaxi.WebAPI/Program.cs
@@ -1,6 +1,15 @@
+using eCommerce.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IProductService, DummyProductService>();
+//builder.Services.AddTransient<IUserService, UserService>();
+//builder.Services.AddTransient<IProductTypeService, ProductTypeService>();
+
+// Configure database
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Server=localhost;Database=eCommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+//builder.Services.AddDatabaseServices(connectionString);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +17,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Ensure database is created
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<eCommerceDbContext>();
+//    dbContext.Database.EnsureCreated();
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
