9fbef00d4a6790d03c82caffc9fa7741f183c024
Ajla Frašto <ajlafrasto@hotmail.com>
2025-04-18 03:00:43 +0200

    CRUD with Interfaces and services without db connected

--- CallTaxi/CallTaxi.WebAPI/Program.cs
+++ CallTaxi/CallTaxi.WebAPI/Program.cs
@@ -1,15 +1,16 @@
 using eCommerce.Services;
+using eCommerce.Services.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddTransient<IProductService, DummyProductService>();
-//builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IUserService, UserService>();
 //builder.Services.AddTransient<IProductTypeService, ProductTypeService>();
 
-// Configure database
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Server=localhost;Database=eCommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
-//builder.Services.AddDatabaseServices(connectionString);
+//Configure database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Server=localhost;Database=CallTaxiDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+builder.Services.AddDatabaseServices(connectionString);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,12 +19,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Ensure database is created
-//using (var scope = app.Services.CreateScope())
-//{
-//    var dbContext = scope.ServiceProvider.GetRequiredService<eCommerceDbContext>();
-//    dbContext.Database.EnsureCreated();
-//}
+//Ensure database is created
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<CallTaxiDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
