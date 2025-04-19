f6176c91a41a8b232edd95e6445a7166bedc22fa
Ajla Frašto <ajlafrasto@hotmail.com>
2025-04-19 02:24:53 +0200

    Generics addition

--- CallTaxi/CallTaxi.WebAPI/Program.cs
+++ CallTaxi/CallTaxi.WebAPI/Program.cs
@@ -4,7 +4,7 @@ using eCommerce.Services.Database;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddTransient<IProductService, DummyProductService>();
+builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IUserService, UserService>();
 //builder.Services.AddTransient<IProductTypeService, ProductTypeService>();
 
